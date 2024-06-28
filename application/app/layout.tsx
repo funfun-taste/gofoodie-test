ae21d11b2b24fe2030bd6c6a9d7eda8d0d3ea4ce
dahoon06 <dahoon226@naver.com>
2024-06-28 23:37:46 +0900

    chore: import path alias 수정

--- application/app/layout.tsx
+++ application/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import '@/styles/reset.scss';
+import "@styles/reset.scss";
 import React from "react";
-import {Layout} from "@/components/Layout";
-import AuthContext from "@/context/AuthContext";
-import MetaHead from "@/components/header/MetaHead";
-import AuthProvider from "@/providers/AuthProvider";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
-import TanstackQueryProviders from "@/providers/TanstackQueryProviders";
+import { Layout } from "@components/Layout";
+import AuthContext from "@context/AuthContext";
+import AuthProvider from "@providers/AuthProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import TanstackQueryProviders from "@providers/TanstackQueryProviders";
 
 export const metadata: Metadata = {
   title: "배~고푸디? 배고플땐 고푸디 | go foodie",
   description: "배~고푸디? 배고플땐 고푸디",
-  keywords: '맛집, 레시피, 동네맛집, 맛, 맛집 리스트, 맛집 정보, 음식, 음식점, 데이트, 푸디, 고푸디, dahoon06, gofoodie, GoFoodie, go-foodie, goFoodie'
+  keywords:
+    "맛집, 레시피, 동네맛집, 맛, 맛집 리스트, 맛집 정보, 음식, 음식점, 데이트, 푸디, 고푸디, dahoon06, gofoodie, GoFoodie, go-foodie, goFoodie",
 };
 
 export default function RootLayout({
@@ -25,9 +25,7 @@ export default function RootLayout({
         <AuthContext>
           <TanstackQueryProviders>
             <AuthProvider>
-              <Layout>
-                {children}
-              </Layout>
+              <Layout>{children}</Layout>
             </AuthProvider>
             <ReactQueryDevtools initialIsOpen={false} />
           </TanstackQueryProviders>
